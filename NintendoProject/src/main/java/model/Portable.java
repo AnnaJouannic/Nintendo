4b13d24c94e1becce6787bf15e090b71e85365ee
Dagmar <cynthia.ehrhard@gmail.com>
2023-07-27 10:54:32 +0200

    commit heritageupdate

--- NintendoProject/src/main/java/model/Portable.java
+++ NintendoProject/src/main/java/model/Portable.java
@@ -1,12 +1,18 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Portable extends Console {
 
-	public Portable(String nom) {
-		super(nom);
+	public Portable(String nom, double prix, LocalDate dateSortie) {
+		super(nom, prix, dateSortie);
+
+	}
+
+	
 		
 	}
 
 	
 	
-}
+
